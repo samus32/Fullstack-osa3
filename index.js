d4d6368c4510f061232dddc7ec2e8646ffeadced
Samu <samu.saronsalo@tuni.fi>
2024-01-17 18:28:17 +0200

    tuotantoversio

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(time.init)
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
   {
